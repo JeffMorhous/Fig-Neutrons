d08fc6cacb2432639e926111dad835057b9ab1ca
Anuj Kothari <anujkothari99@gmail.com>
2020-01-26 18:35:54 -0500

    added hint where one of the cards of a set is given

--- lab2/game.rb
+++ lab2/game.rb
@@ -46,7 +46,7 @@ class Game < Array
     ask_question
     answer = gets.to_i
     until answer.between?(1, 4)
-      print 'Try again. Choose 1, 2, 3 or 4:'
+      print 'Try again. Choose 1, 2, 3 or 4: '
       answer = gets.to_i
     end
     find_a_set if answer == 1
@@ -59,10 +59,10 @@ class Game < Array
   def ask_question
     puts 'What would you like to do?'
     puts '1. Pick a set.'
-    puts '2. Get a hint: How many matches are there?'
+    puts '2. Get a hint.'
     puts '3. Shuffle the board.'
     puts '4. End Game'
-    print 'Choose 1, 2, 3 or 4:'
+    print 'Choose 1, 2, 3 or 4: '
   end
 
   # steps for player to select three cards and check if it is a set
@@ -82,10 +82,10 @@ class Game < Array
   def pick_a_set
     cards = []
     (1..3).each do |i|
-      print "Choose card #{i}:"
+      print "Choose card #{i}: "
       answer = gets.to_i
       until answer.between?(1, @board.length) && !cards.include?(@board[answer - 1])
-        puts "Try again! Choose card #{i}:"
+        puts "Try again! Choose card #{i}: "
         answer = gets.to_i
       end
       cards << @board[answer - 1]
@@ -123,7 +123,22 @@ class Game < Array
   def give_hint
     @score -= 1
     matches = find_matches.length
-    puts "\nThere #{matches == 1 ? 'is 1 match' : "are #{matches} matches"}."
+    card = find_matches.first.first
+    cardIndex =  @board.find_index(card) + 1
+    puts "Hint Types:"
+    puts "1. Number of matching sets"
+    puts "2. One card in a set"
+    print "Choose what kind of hint you want: "
+    answer = gets.to_i
+    until answer.between?(1, 2)
+      print 'Try again. Choose 1 or 2: '
+      answer = gets.to_i
+    end
+    puts "\nHint: There #{matches == 1 ? 'is 1 match' : "are #{matches} matches"}. " if answer == 1
+    puts "\nHint: Card #{cardIndex} belongs to a set. " if answer == 2
+
+
+    
     puts ''
     next_move
   end
@@ -180,27 +195,27 @@ class Game < Array
 
   # printing the shapes.  Work in progress.
   def shape_string(shape, fill, number)
-    # shapes1 = { filled_circle: "\u25CF".encode('utf-8'),
-    #             half_circle: "\u25D0".encode('utf-8'),
-    #             open_circle: "\u25CB".encode('utf-8'),
-    #             filled_tri: "\u25B2".encode('utf-8'),
-    #             half_tri: "\u25ED".encode('utf-8'),
-    #             open_tri: "\u25B3".encode('utf-8'),
-    #             filled_square: "\u25A0".encode('utf-8'),
-    #             half_square: "\u25E7".encode('utf-8'),
-    #             open_square: "\u25A1".encode('utf-8') }
-    shapes2 = { filled_circle: "\u25CD".encode('utf-8'),
-                half_circle: "\u25C9".encode('utf-8'),
-                open_circle: "\u274D".encode('utf-8'),
-                filled_tri: "\u25C6".encode('utf-8'),
-                half_tri: "\u25C8".encode('utf-8'),
-                open_tri: "\u25C7".encode('utf-8'),
-                filled_square: "\u25A9".encode('utf-8'),
-                half_square: "\u25A3".encode('utf-8'),
-                open_square: "\u2610".encode('utf-8') }
+    shapes1 = { filled_circle: "\u25CF".encode('utf-8'),
+                half_circle: "\u25D0".encode('utf-8'),
+                open_circle: "\u25CB".encode('utf-8'),
+                filled_tri: "\u25B2".encode('utf-8'),
+                half_tri: "\u25ED".encode('utf-8'),
+                open_tri: "\u25B3".encode('utf-8'),
+                filled_square: "\u25A0".encode('utf-8'),
+                half_square: "\u25E7".encode('utf-8'),
+                open_square: "\u25A1".encode('utf-8') }
+    # shapes2 = { filled_circle: "\u25CD".encode('utf-8'),
+    #             half_circle: "\u25C9".encode('utf-8'),
+    #             open_circle: "\u274D".encode('utf-8'),
+    #             filled_tri: "\u25C6".encode('utf-8'),
+    #             half_tri: "\u25C8".encode('utf-8'),
+    #             open_tri: "\u25C7".encode('utf-8'),
+    #             filled_square: "\u25A9".encode('utf-8'),
+    #             half_square: "\u25A3".encode('utf-8'),
+    #             open_square: "\u2610".encode('utf-8') }
     stripe = []
     number.times do
-      stripe << shapes2["#{fill}_#{shape}".to_sym]
+      stripe << shapes1["#{fill}_#{shape}".to_sym]
     end
     stripe.join(' ')
   end
