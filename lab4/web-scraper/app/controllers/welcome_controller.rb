1b2cd400c9f1bcf5b671083767d51d2a99cbe2d0
Anuj Kothari <anujkothari99@gmail.com>
2020-02-17 01:45:08 -0500

    Scrape CSE course info website and print out necessary data for each section of a course

--- lab4/web-scraper/app/controllers/welcome_controller.rb
+++ lab4/web-scraper/app/controllers/welcome_controller.rb
@@ -5,8 +5,26 @@ class WelcomeController < ApplicationController
   def index
     agent = Mechanize.new
     page = agent.get('https://web.cse.ohio-state.edu/oportal/schedule_display/')
-    page.links.each do |link|
-      puts link.text
+    
+    hashMap = Hash.new
+    links = page.css('a')
+    links.each do |item| 
+      courseNumber = item.attribute('href')
+      courseTitle = item.text
+      hashMap[courseNumber.to_s] = courseTitle
+    end
+
+    tables = page.css("tr.group0, tr.group1")
+    tables.each do |item|
+      puts "Course Title:" + hashMap["#" + item.parent.parent.parent.attribute('id')]
+      sectionData = item.text.split("\n")
+      puts "Class number: " + sectionData[1]
+      puts "Component: " + sectionData[2]
+      puts "Location: " + sectionData[3]
+      puts "Times: " + sectionData[4]
+      puts "Instructor: " + sectionData[5]
+      puts "Session: " + sectionData[6]
+      puts "\n"
     end
   end
 end
