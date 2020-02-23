d3ebb6a4e818e993696a74a6ceed2871d29a2bab
Anuj Kothari <anujkothari99@gmail.com>
2020-02-23 17:28:47 -0500

    added comments and changed naming convention to snake case

--- lab4/web-scraper/db/seeds.rb
+++ lab4/web-scraper/db/seeds.rb
@@ -10,23 +10,31 @@ require 'mechanize'
 
 Course.delete_all
 agent = Mechanize.new
+
+# Use mechanize to scrape the OSU CSE course information website
 page = agent.get('https://web.cse.ohio-state.edu/oportal/schedule_display/')
 
-hashMap = Hash.new
+hash_map = Hash.new
+
+# Retrieve all the links and for each link find its id (course number) and title (course title)
 links = page.css('a')
 links.each do |item|
-  courseNumber = item.attribute('href')
-  courseTitle = item.text
-  hashMap[courseNumber.to_s] = courseTitle
+  course_number = item.attribute('href')
+  course_title = item.text
+
+  # Create a hash that maps the course number to the course title
+  hash_map[course_number.to_s] = course_title
 end
 
+# Find all the rows for each table to get the information for each section of a course
 tables = page.css("tr.group0, tr.group1")
 tables.each do |item|
-sectionData = item.text.split("\n")
-Course.create(number: sectionData[1],
-              title: hashMap["#" + item.parent.parent.parent.attribute('id')],
-              lab: sectionData[2].include?("LAB"),
-              location: sectionData[3],
-              time: sectionData[4],
-              instructor: sectionData[5])
+  # Use the course id to find the title of the section's course
+  section_data = item.text.split("\n")
+  Course.create(number: section_data[1],
+                title: hash_map["#" + item.parent.parent.parent.attribute('id')],
+                lab: section_data[2].include?("LAB"),
+                location: section_data[3],
+                time: section_data[4],
+                instructor: section_data[5])
 end
