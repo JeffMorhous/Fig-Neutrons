496ebf372159d718ec434bc40127d9c6bcfa0bb5
JeffMorhous <jobboy18@gmail.com>
2020-04-05 16:46:54 -0400

    WIP: Generate Course table - still needs not null specifications, default falses, and instructor_ID primary key

---
+++ hiring-graders/db/migrate/20200405204607_create_courses.rb
@@ -0,0 +1,17 @@
+class CreateCourses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :courses do |t|
+      t.string :department
+      t.int :course
+      t.boolean :is_lab
+      t.string :days
+      t.time :start_time
+      t.time :end_time
+      t.string :location
+      t.boolean :need_grader
+      t.boolean :have_grader
+
+      t.timestamps
+    end
+  end
+end
