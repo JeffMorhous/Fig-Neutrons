c4672e2fac4e9108b0f3208c96412549adc26597
JeffMorhous <jobboy18@gmail.com>
2020-04-22 19:55:16 -0400

    Fix (or delete if unneeded) existing tests generated by rails

--- hiring-graders/test/controllers/admin_controller_test.rb
+++ hiring-graders/test/controllers/admin_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  test "should get signup" do
-    get admin_signup_url
-    assert_response :success
-  end
 
 end
--- hiring-graders/test/controllers/instructor_controller_test.rb
+++ hiring-graders/test/controllers/instructor_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class InstructorControllerTest < ActionDispatch::IntegrationTest
-  test "should get signup" do
-    get instructor_signup_url
-    assert_response :success
-  end
 
 end
