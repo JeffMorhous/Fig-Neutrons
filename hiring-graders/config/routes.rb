a37520d3cd2adfe0917ebb45ba29b028fc1e2065
marshall-osu <tanner.marshall2000@gmail.com>
2020-04-21 23:16:41 -0400

    Added styling for the admin pages along with adding the option to remove a grader from a course

--- hiring-graders/config/routes.rb
+++ hiring-graders/config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   get 'admin/dashboard'
   post 'admin/section'
   post 'admin/select'
+  post 'admin/delete'
 
   # Student Routes
   get 'student/application' # Routes to retrieve the student grader application
