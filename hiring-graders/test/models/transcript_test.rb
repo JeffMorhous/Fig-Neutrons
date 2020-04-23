6ce770a1a26c6747db900867fbb6cefb540e058b
JeffMorhous <jobboy18@gmail.com>
2020-04-22 21:47:16 -0400

    Add note to Transcript model test

--- hiring-graders/test/models/transcript_test.rb
+++ hiring-graders/test/models/transcript_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class TranscriptTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  #Transcript does not need tests, as it does not have custom
+  #business logic, and testing the accuracy of ActiveRecord is not recommended
 end
