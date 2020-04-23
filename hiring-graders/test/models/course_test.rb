b7d5e697345cfcf131d2cb935629e767c7e8bba2
JeffMorhous <jobboy18@gmail.com>
2020-04-22 21:43:05 -0400

    Add note to Course model test

--- hiring-graders/test/models/course_test.rb
+++ hiring-graders/test/models/course_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  #Course does not need tests, as it does not have custom
+  #business logic, and testing the accuracy of ActiveRecord is not recommended
 end
